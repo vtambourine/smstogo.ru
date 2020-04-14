6bc45758c4f4853fc2a7746c6050083f916fc70a
Veniamin (Benjamin) Kleshchenkov <veniamin.kleshchenkov@dazn.com>
2020-04-14 23:24:18 +0300

    Publish 0.1

---
+++ precache-manifest.42052c046cf6837608b37782a6a9eff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6547ba2350200ec483d2e82d665a3b28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "801ee1de0f5ec79367da",
+    "url": "/static/css/main.1a0377b9.chunk.css"
+  },
+  {
+    "revision": "d7f5bbea562d213e2712",
+    "url": "/static/js/2.1b60219c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1b60219c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801ee1de0f5ec79367da",
+    "url": "/static/js/main.03a13cef.chunk.js"
+  },
+  {
+    "revision": "54cc187cf46ac0a698a1",
+    "url": "/static/js/runtime-main.be068c87.js"
+  }
+]);
